489b2ba1dbc206b42d08e661d49a968574b095bb
Harald Partmann <harald.partmann@aon.at>
2016-02-22 23:45:29 +0100

    add project Task2

---
+++ tasks/Task2/Task2/Haus.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Net;
+
+namespace Task2
+{
+    public class Haus
+    {
+        private decimal Fixpreis = 250000;
+        private decimal member_Preis;
+
+        /// <summary>
+        /// Erzeugt ein neues Haus
+        /// </summary>
+        /// <param name="Dach"></param>
+        /// <param name="Fenster"></param>
+        /// <param name="Türen"></param>
+        /// <param name="Preis"></param>
+        /// <param name="Zimmer"></param>
+        public Haus(string inputDach, string inputFenster, string inputTüren, int inputZimmer, decimal inputPreis)
+        {
+            if (string.IsNullOrEmpty(inputDach)) throw new ArgumentException("Dach muss vorhanden sein.", nameof(inputDach));
+            if (string.IsNullOrEmpty(inputFenster)) throw new ArgumentException("Fenster müssen vorhanden sein.", nameof(inputFenster));
+            if (string.IsNullOrEmpty(inputTüren)) throw new ArgumentException("Türen müssen vorhanden sein.", nameof(inputTüren));
+            if (inputZimmer <= 0) throw new ArgumentException("Zimmer müssen vorhanden sein.", nameof(inputZimmer));
+            this.Dach = inputDach;
+            this.Fenster = inputFenster;
+            Türen = inputTüren;
+            Zimmer = inputZimmer;
+            UpdatePreis(inputPreis);
+        }
+
+        public string Dach { get; }
+
+        public string Fenster { get; }
+
+        public string Türen { get; }
+
+        public int Zimmer { get; set; }
+
+        public decimal Preis
+        {
+            get { return member_Preis; }
+            set
+            {
+                if (Zimmer < 4) member_Preis = Fixpreis;
+                else
+                {
+                    member_Preis = value;
+                }
+
+            }
+        }
+
+        public void UpdatePreis(decimal neuerPreis)
+        {
+            if (neuerPreis < 0) throw new ArgumentException("Preis darf nicht negativ sein.", nameof(neuerPreis));
+            member_Preis = neuerPreis;
+        }
+    }
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Haus Regina = new Haus("Prefa", "Josko", "Josko", 5, 350000);
+            Haus Sonja = new Haus("Velux", "Internorm", "Josko", 6, 450000);
+            Haus Mathilde = new Haus("Bramac", "Internorm", "Eckmaier", 7, 800000);
+            Console.WriteLine("Haustyp: Dach|Fenster|Türen|Zimmer|Preis in Euro");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Sonja: {0}|{1}|{2}|{3}|{4}", Sonja.Dach, Sonja.Fenster, Sonja.Türen, Sonja.Zimmer, Sonja.Preis);
+            Console.WriteLine("Mathilde: {0}|{1}|{2}|{3}|{4}", Mathilde.Dach, Mathilde.Fenster, Mathilde.Türen, Mathilde.Zimmer, Mathilde.Preis);
+            Console.WriteLine("Regina: {0}|{1}|{2}|{3}|{4}", Regina.Dach, Regina.Fenster, Regina.Türen, Regina.Zimmer, Regina.Preis);
+            Regina.Zimmer = 4;
+            Regina.UpdatePreis(200000);
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Neue Zimmeranzahl für Haus Regina: {0}", Regina.Zimmer);
+            Console.WriteLine("Neuer Preis für Haus Regina: {0}", Regina.Preis);
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Regina: {0}|{1}|{2}|{3}|{4}", Regina.Dach, Regina.Fenster, Regina.Türen, Regina.Zimmer, Regina.Preis);
+            Sonja.Zimmer = 3;
+            Console.WriteLine("Sonja: {0}|{1}|{2}|{3}|{4}", Sonja.Dach, Sonja.Fenster, Sonja.Türen, Sonja.Zimmer, Sonja.Preis);
+        }
+    }
+}
